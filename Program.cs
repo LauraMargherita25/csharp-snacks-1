9e34b370111407dddf1cc5c2d20dfd79ca676619
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-06-27 14:46:28 +0200

    Snack 4

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-// Snack 3) Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
-
 int sum = 0;
-
+double avg;
 for (int i = 1; i <= 10; i++)
 {
 
     int numeroUtente = int.Parse(Console.ReadLine());
     sum += numeroUtente;
 }
-Console.Write("La somma : {0} " + sum);
+avg = sum / 10.0;
+Console.Write("La somma : {0}/n la media: {1}", sum,avg);
+
+
+// Snack 3) Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+//int sum = 0;
+
+//for (int i = 1; i <= 10; i++)
+//{
+
+//    int numeroUtente = int.Parse(Console.ReadLine());
+//    sum += numeroUtente;
+//}
+//Console.Write("La somma : {0} " + sum);
