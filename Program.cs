2b1a62f369cfbd340789ae2804bcc132c9b3c553
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-06-27 14:41:47 +0200

    Snak 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Snack 3) Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+int sum = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+
+    int numeroUtente = int.Parse(Console.ReadLine());
+    sum += numeroUtente;
+}
+Console.Write("La somma : {0} " + sum);
