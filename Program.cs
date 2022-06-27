0c039d9ca96254fed91173069b5c2ea0ff95a74d
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-06-27 14:56:58 +0200

    Snack 5

--- Program.cs
+++ Program.cs
@@ -1,14 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int sum = 0;
-double avg;
-for (int i = 1; i <= 10; i++)
-{
 
-    int numeroUtente = int.Parse(Console.ReadLine());
-    sum += numeroUtente;
+// Snack 5) chiedi all'uente di inserire un numero se è pari stampa se è dispari stampa il numero successivo
+
+Console.WriteLine("scrivi un numero");
+int numeroUtente = int.Parse(Console.ReadLine());
+if (numeroUtente % 2 == 0)
+{
+    Console.WriteLine(numeroUtente);
 }
-avg = sum / 10.0;
-Console.Write("La somma : {0}/n la media: {1}", sum,avg);
+else
+{
+    Console.WriteLine("scrivi un numero");
+};
+
+// Snack 4) calcola somma e media.
+//int sum = 0;
+//double avg;
+//for (int i = 1; i <= 10; i++)
+//{
+
+//    int numeroUtente = int.Parse(Console.ReadLine());
+//    sum += numeroUtente;
+//}
+//avg = sum / 10.0;
+//Console.Write("La somma : {0}/n la media: {1}", sum,avg);
 
 
 // Snack 3) Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
